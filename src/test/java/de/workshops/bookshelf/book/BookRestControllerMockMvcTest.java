b9048901b169550047bf20f78324e1e12ccf63d4
Birgit Kratz <mail@birgitkratz.de>
2024-01-14 21:59:35 +0100

    Tag 3: Test mit neuem SpringBoot3 ResClient

--- src/test/java/de/workshops/bookshelf/book/BookRestControllerMockMvcTest.java
+++ src/test/java/de/workshops/bookshelf/book/BookRestControllerMockMvcTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
@@ -15,12 +14,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest
 @AutoConfigureMockMvc
 class BookRestControllerMockMvcTest {
-
-    @LocalServerPort
-    int port;
 
     @Autowired
     MockMvc mockMvc;
---
+++ src/test/java/de/workshops/bookshelf/book/BookRestControllerRestClientTest.java
@@ -0,0 +1,113 @@
+package de.workshops.bookshelf.book;
+
+import jakarta.annotation.PostConstruct;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestClient;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.assertj.core.api.Assertions.catchThrowableOfType;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class BookRestControllerRestClientTest {
+
+    @LocalServerPort
+    private int port;
+
+    @MockBean
+    private BookService bookService;
+
+    private RestClient restClient;
+
+    @PostConstruct
+    void initRestClient() {
+        restClient = RestClient.builder()
+                .baseUrl("http://localhost:%d/book".formatted(port))
+                .build();
+    }
+
+    @Test
+    void shouldGetAllBooks() {
+        when(bookService.getAllBooks()).thenReturn(List.of(new Book(), new Book(), new Book()));
+
+        final var responseEntity = restClient
+                .get()
+                .retrieve()
+                .toEntity(List.class);
+
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).hasSize(3);
+    }
+
+    @Test
+    void shouldGetBookByIsbn_OK() throws BookException {
+        final var isbn = "123456789";
+
+        when(bookService.getBookByIsbn(isbn)).thenAnswer(invocationOnMock -> {
+            var book = new Book();
+            book.setIsbn(isbn);
+            return book;
+        });
+
+        final var responseEntity = restClient
+                .get()
+                .uri("/%s".formatted(isbn))
+                .retrieve()
+                .toEntity(Book.class);
+
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody())
+                .isNotNull()
+                .hasFieldOrPropertyWithValue("isbn", isbn);
+    }
+
+    @Test
+    void shouldGetBookByIsbn_I_AM_A_TEAPOT() throws BookException {
+        doThrow(BookException.class).when(bookService).getBookByIsbn(anyString());
+
+        final var exception = catchThrowableOfType(() -> restClient
+                .get()
+                .uri("/123456789")
+                .retrieve()
+                .toEntity(Book.class), HttpClientErrorException.class);
+
+        assertThat(exception.getStatusCode()).isEqualTo(HttpStatus.I_AM_A_TEAPOT);
+
+        final var problemDetail = exception.getResponseBodyAs(ProblemDetail.class);
+        assertThat(problemDetail).isNotNull()
+                .hasFieldOrPropertyWithValue("title", "Book not found.");
+    }
+
+    @Test
+    void shouldGetBookByAuthor_BAD_REQUEST() {
+        final var exception = catchThrowableOfType(() -> restClient
+                .get()
+                .uri("?author=Ro")
+                .retrieve()
+                .toEntity(Book.class), HttpClientErrorException.class);
+
+        assertThat(exception.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+    }
+
+    @Test
+    void shouldGetBookByAuthor_BAD_REQUEST_differentWayOfAssertJAssertion() {
+        assertThatExceptionOfType(HttpClientErrorException.class)
+                .isThrownBy(() -> restClient
+                        .get()
+                        .uri("?author=Ro")
+                        .retrieve()
+                        .toEntity(Book.class))
+                .satisfies(e -> assertThat(e.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST));
+    }
+}
